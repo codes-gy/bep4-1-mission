889b91f3e445f15183fe0d111683f24e40dfa3c5
codes-gy <podojjang_kr@naver.com>
2026-09-17 12:56:46 +0900

    0002

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO member (username, password, nickname, create_date, modify_date) VALUES ('system', '1234', '시스템', NOW(), NOW());
-INSERT INTO member (username, password, nickname, create_date, modify_date) VALUES ('holding', '1234', '홀딩', NOW(), NOW());
-INSERT INTO member (username, password, nickname, create_date, modify_date) VALUES ('admin', '1234', '관리자', NOW(), NOW());
-INSERT INTO member (username, password, nickname, create_date, modify_date) VALUES ('user1', '1234', '사용자1', NOW(), NOW());
-INSERT INTO member (username, password, nickname, create_date, modify_date) VALUES ('user2', '1234', '사용자2', NOW(), NOW());
-INSERT INTO member (username, password, nickname, create_date, modify_date) VALUES ('user3', '1234', '사용자3', NOW(), NOW());
