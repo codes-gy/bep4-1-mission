dfc07ac7bef5b68fa0a5b6214c0422d1fd7b09c6
codes-gy <podojjang_kr@naver.com>
2026-09-18 16:33:16 +0900

    0018

---
+++ src/main/java/com/back/boundedContext/member/domain/ReplicaMember.java
@@ -0,0 +1,25 @@
+package com.back.boundedContext.member.domain;
+
+import com.back.shared.member.domain.BaseMember;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+public abstract class ReplicaMember extends BaseMember {
+    @Id
+    private int id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
+    public ReplicaMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
+}
