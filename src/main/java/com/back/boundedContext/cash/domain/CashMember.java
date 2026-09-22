ffbf972900f29c0d143f157e55d571cad2cd49ca
codes-gy <podojjang_kr@naver.com>
2026-09-22 14:45:27 +0900

    0039

--- src/main/java/com/back/boundedContext/cash/domain/CashMember.java
+++ src/main/java/com/back/boundedContext/cash/domain/CashMember.java
@@ -1,6 +1,7 @@
 package com.back.boundedContext.cash.domain;
 
 import com.back.boundedContext.member.domain.ReplicaMember;
+import com.back.shared.cash.dto.CashMemberDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -15,5 +16,16 @@ import java.time.LocalDateTime;
 public class CashMember extends ReplicaMember {
     public CashMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname, int activityScore) {
         super(id, createDate, modifyDate, username, password, nickname, activityScore);
+    }
+
+    public CashMemberDto toDto() {
+        return new CashMemberDto(
+                getId(),
+                getCreateDate(),
+                getModifyDate(),
+                getUsername(),
+                getNickname(),
+                getActivityScore()
+        );
     }
 }
